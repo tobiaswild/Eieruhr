a2e3a18ba9f67e562872a04d8f77858b92d70cd9
BuildTools <46871747+Tobias447@users.noreply.github.com>
2021-08-11 19:45:42 +0200

    files

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let alarm = new Audio('sound/alarm.mp3');
+let timerStarted = false;
+
+function startTimer() {
+    if (!timerStarted) {
+        let startTime = new Date().getTime();
+        let fiveMinutes = 1000 * 60 * 5;
+        let endTime = startTime + fiveMinutes;
+
+        setInterval(function () {
+            let timeLeft = endTime - new Date().getTime();
+            if (timeLeft > 0) {
+                let minutes = timeLeft / (1000 * 60);
+                minutes = Math.floor(minutes);
+                let seconds = timeLeft / 1000 % 60;
+                seconds = Math.round(seconds);
+                seconds = ('0' + seconds).slice(-2);
+                let text = '0' + minutes + ' : ' + seconds;
+                timer.innerHTML = text;
+            } else {
+                alarm.play();
+                let text = '00 : 00';
+                timer.innerHTML = text;
+            }
+        }, 1000)
+        timerStarted = true;
+    }
+}
